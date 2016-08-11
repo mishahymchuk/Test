a03005fae678c11ad110d8424038f56c9a4a3be7
Misha Hymchuk <misha.hymchuk@gmail.com>
2016-08-11 17:55:01 +0300

    vehicle

---
+++ vehicle_methods.rb
@@ -0,0 +1,18 @@
+def accelerate
+  puts "Steping on the gas"
+  puts "Speeding up"
+end
+
+def sound_horn
+  puts "Ptrssing the button"
+  puts "Beep beep!"
+end
+
+def use_headlights (brightness = "high-beam")
+  puts "Turning on #{brightness} headlights"
+  puts "Watch out for deer!"
+end
+
+sound_horn
+accelerate
+use_headlights  
